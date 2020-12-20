bb80e82a528595f4e4fd4f89fcb54a89ef924ca2
Soso <sophie.abramian@gmail.com>
2020-12-20 23:31:05 +0100

    starting again

--- pySAM/pySAM/simulation.py
+++ pySAM/pySAM/simulation.py
@@ -8,18 +8,22 @@ from netcdf4 import Dataset
 class Simulation:
 	def __init__(self, 'run'=run, 'velocity'=velocity, 'depth_shear'=depth_shear):
 		
-		self.velocity=velocity
-		self.depth_shear=depth_shear
-		self.run=run
+		self.velocity = velocity
+		self.depth_shear = depth_shear
+		self.run = run
 
-		self.path_field_1D=f'/Users/sophieabramian/Desktop/SAM_project/data/{self.run}/1D_FILES/U{self.velocity}_H{self.depth_shear}'
-		self.path_field_2D='..'
-		self.path_field_3D='..'
+		self.path_field_1D = f'/Users/sophieabramian/Desktop/SAM_project/data/{self.run}/1D_FILES/U{self.velocity}_H{self.depth_shear}'
+		self.path_field_2D = '..'
+		self.path_field_3D = '..'
 
-		self.dataset_1D=xr.open_dataset(set.path_field_1D, decode_cf=False)
-		self.dataset_2D=xr.open_dataset(set.path_field_2D, decode_cf=False)
-		self.dataset_3D=xr.open_dataset(set.path_field_3D, decode_cf=False)
+		self.dataset_1D = xr.open_dataset(set.path_field_1D, decode_cf=False)
+		self.dataset_2D = xr.open_dataset(set.path_field_2D, decode_cf=False)
+		self.dataset_3D = xr.open_dataset(set.path_field_3D, decode_cf=False)
+
+		self.dataset_1D.close()
+		self.dataset_2D.close()
+		self.dataset_3D.close()
 
 
 
-self.squall_line=SquallLine()
+self.squall_line = SquallLine('PW'=self.dataset_2D.PW, 'Preci'=self.dataset_2D.PRECi, 'U'=self.dataset_3D.U, 'V'=self.dataset_3D.V, 'W'=self.dataset_3D.W)
